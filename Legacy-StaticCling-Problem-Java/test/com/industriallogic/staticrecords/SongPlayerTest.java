bc8fb2ca4de4df394e158bb4dd3f1ecbd03c249e
brliu <boraliu@gmail.com>
2014-05-05 10:28:42 +0800

    Characteristics Test 2: Play Song (2nd public API), resulting in test coverage of 65.1%.

--- Legacy-StaticCling-Problem-Java/test/com/industriallogic/staticrecords/SongPlayerTest.java
+++ Legacy-StaticCling-Problem-Java/test/com/industriallogic/staticrecords/SongPlayerTest.java
@@ -1,10 +1,13 @@
 package com.industriallogic.staticrecords;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.internal.runners.statements.Fail;
 
 public class SongPlayerTest {
 
@@ -24,6 +27,27 @@ public class SongPlayerTest {
 		SongPlayer songPlayer = new SongPlayer(playerName, statistics);
 
 		assertNotNull(songPlayer);
+		assertEquals(playerName, songPlayer.getPlayerName());
 	}
 
+	@Test
+	public void playOneSong() {
+		String playerName = "player 1";
+		SongPlayerStatistics statistics = new SongPlayerStatistics();
+
+		SongPlayer songPlayer = new SongPlayer(playerName, statistics);
+
+		String songName = "song 1";
+		int dataLength = 256;
+		Song oneSong = new Song(songName, dataLength);
+		songPlayer.addSong(oneSong);
+
+		try {
+			songPlayer.playSong(songName);
+		} catch (SongNotFoundException e) {
+			fail();
+		}
+
+		assertEquals(1, songPlayer.getNumberOfSongsPlayed());
+	}
 }
